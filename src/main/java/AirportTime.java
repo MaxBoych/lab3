2b63eb09eadb3779113b764d56c1a4b95e50fdf1
maxboychuk <maxboydad@gmail.com>
2019-11-01 21:11:45 +0300

    Scripted auto-commit on change (2019-11-01 21:11:45) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -5,12 +5,17 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportTime {
+
+    
+
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf().setAppName("lab3");
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
 
+        JavaRDD<String[]> flights = sparkContext.textFile()
     }
 }
--- src/main/java/UtilitiesCSV.java
+++ src/main/java/UtilitiesCSV.java
@@ -1,12 +1,6 @@
 public class UtilitiesCSV {
     private static final String[] EMPTY = {};
-    private static final int CANCELED = 19;
-    private static final int DELAYED = 18;
-    private static final int AIRPORT_KEY = 0;
-    private static final int FLIGHT_KEY = 1;
     private static final int FIRST_ITEM = 0;
-
-    private static final int DEST_AIRPORT_INDEX = 14;
 
     public static String[] parseAndFilter(String line) {
         return removeQuotes(filterFlightData(parseFlightData(line)));
