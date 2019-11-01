f9fa120a865531fea04a5ef9f7303fbcd261c76d
maxboychuk <maxboydad@gmail.com>
2019-11-01 20:30:32 +0300

    Scripted auto-commit on change (2019-11-01 20:30:32) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -4,30 +4,13 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportTime {
-    public static void main(String[] args) throws Exception {
-        if (args.length != 3) {
-            System.exit(-1);
-        }
-
-        Job job = Job.getInstance();
-        job.setJarByClass(AirportTime.class);
-
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
-
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-
-        job.setPartitionerClass(DataPartitioner.class);
-        job.setGroupingComparatorClass(AirportGroupingComparator.class);
-        job.setReducerClass(AirportReducer.class);
-        job.setMapOutputKeyClass(AirportWritableComparable.class);
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(2);
-
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        
     }
 }
