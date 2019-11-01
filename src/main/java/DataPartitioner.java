2d1fa68b3fd6f97db2572dedc67aebcabc8d6ee5
maxboychuk <maxboydad@gmail.com>
2019-11-01 22:51:03 +0300

    Scripted auto-commit on change (2019-11-01 22:51:03) by gitwatch.sh

--- src/main/java/DataPartitioner.java
+++
@@ -1,9 +0,0 @@
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
-
-public class DataPartitioner extends HashPartitioner<AirportWritableComparable, Text> {
-    @Override
-    public int getPartition(AirportWritableComparable key, Text value, int numReduceTasks) {
-        return (Integer.valueOf(key.getAirportID()).hashCode() & Integer.MAX_VALUE) % numReduceTasks;
-    }
-}
