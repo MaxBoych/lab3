52bd7ab0c8062a571265b4d8d6434306b0d3160d
maxboychuk <maxboydad@gmail.com>
2019-11-02 01:23:54 +0300

    Scripted auto-commit on change (2019-11-02 01:23:54) by gitwatch.sh

--- src/main/java/FlightInfo.java
+++ src/main/java/FlightInfo.java
@@ -1,2 +1,9 @@
 public class FlightInfo {
+    private int delayed;
+    private int cancelled;
+
+    public FlightInfo(int delayed, int cancelled) {
+        this.delayed = delayed;
+        this.cancelled = cancelled;
+    }
 }
