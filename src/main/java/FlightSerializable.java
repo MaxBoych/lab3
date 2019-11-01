0c81e57179012bf369f75784110d0fe37ca58b8a
maxboychuk <maxboydad@gmail.com>
2019-11-02 01:51:08 +0300

    Scripted auto-commit on change (2019-11-02 01:51:08) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,2 +1,33 @@
-public class FlightSerializable {
+import java.io.Serializable;
+
+public class FlightSerializable implements Serializable {
+
+    private int flightsAmount;
+    private int flightsDelayed;
+    private int delayMaxValue;
+    private int flightsCancelled;
+
+    public FlightSerializable(int flightsAmount, int flightsDelayed,
+                              int delayMaxValue, int flightsCancelled) {
+        this.flightsAmount = flightsAmount;
+        this.flightsDelayed = flightsDelayed;
+        this.delayMaxValue = delayMaxValue;
+        this.flightsCancelled = flightsCancelled;
+    }
+
+    public int getFlightsAmount() {
+        return this.flightsAmount;
+    }
+
+    public int getFlightsDelayed() {
+        return this.flightsDelayed;
+    }
+
+    public int getDelayMaxValue() {
+        return this.delayMaxValue;
+    }
+
+    public int getFlightsCancelled() {
+        return this.flightsCancelled;
+    }
 }
