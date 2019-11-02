a9047f6953e70b413f23aed361d94fa32b20b316
maxboychuk <maxboydad@gmail.com>
2019-11-03 01:54:09 +0300

    Scripted auto-commit on change (2019-11-03 01:54:09) by gitwatch.sh

--- src/main/java/UtilitiesCSV.java
+++ src/main/java/UtilitiesCSV.java
@@ -1,5 +1,5 @@
 public class UtilitiesCSV {
-    private static final String[] EMPTY = {};
+    private static final String[] EMPTY = new String[2];
     private static final int CANCELED = 19;
     private static final int DELAYED = 18;
     private static final int AIRPORT_KEY = 0;
